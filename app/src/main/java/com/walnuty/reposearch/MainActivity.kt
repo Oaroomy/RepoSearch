b3a1401915240b41a1e88147fdaf83d4b00e0265
Oaroomy <walnuty87@gmail.com>
2022-06-14 12:56:40 +0900

    [Build] #1_setting project
    - 프로젝트 신규 생성
    - databinding, viewmodel, livedata, coroutine build.gradle 추가

---
+++ app/src/main/java/com/walnuty/reposearch/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.walnuty.reposearch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
